c812e04da737e4b048c6bf251ca07bf8e276edf5
Lars Vadgaard <lars@vadg.io>
2022-11-18 03:24:40 +0000

    async

--- src/SRL/Interface.hs
+++ src/SRL/Interface.hs
@@ -1,4 +1,3 @@
-
 module SRL.Interface (
     runProgram,
     invertProgram,
@@ -7,6 +6,7 @@ module SRL.Interface (
 
 import Common.Log
 
+
 import qualified RL.AST as RL
 
 import SRL.Parser
@@ -14,17 +14,17 @@ import SRL.Interp
 import SRL.Inversion
 import SRL.Translation
 
-runProgram :: String -> (Either Error VarTab, Log)
+runProgram :: String -> IO (Either Error VarTab, Log)
 runProgram source =
   case parseProgram source of
-    Left err -> (Left err, emptyLog)
-    Right program -> runAST program
+    Left err -> return (Left err, emptyLog)
+    Right program -> return $ runAST program
 
-invertProgram :: String -> Either Error SRLProgram
-invertProgram source = handleSource source invert
+invertProgram :: String -> IO (Either Error SRLProgram)
+invertProgram source = return $ handleSource source invert
 
-translateProgram :: String -> Either Error RL.RLProgram
-translateProgram source = handleSource source translate
+translateProgram :: String -> IO (Either Error RL.RLProgram)
+translateProgram source = return $ handleSource source translate
 
 handleSource :: String -> (SRLProgram -> a) -> Either Error a
 handleSource source handler =
