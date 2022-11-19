8759aaf2c4038376662f85c72f7577f74ac7e792
Lars Vadgaard <lars@vadg.io>
2022-11-19 13:37:13 +0000

    share

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -143,8 +143,8 @@
                 errorwindow.append('<p>Failed to shorten the URL. Note that (encoded) URLs longer than 5,000 characters cannot be shortened.</p>');
             }).
             always(function() {
-                errorwindow.append("<p>Here is your link: <a href='" + url + "'>" + url + "</a></p>") ;
-                errorwindow.append("<p><i>If you're especially proud of this program, consider contributing to the list of example programs by sending the URL to <a href='mailto:&#114;&#101;&#118;&#64;&#118;&#97;&#100;&#103;&#46;&#105;&#111;?subject=" + encodeURIComponent('RL/SRL Program') + "&body=" + encodeURIComponent(url) + "'>&#114;&#101;&#118;&#64;&#118;&#97;&#100;&#103;&#46;&#105;&#111;</a>.</i></p>") ;
+                errorwindow.append("<p>Here is your link: <a href='" + url + "' target='_blank'>" + url + "</a></p>") ;
+                errorwindow.append("<p><i>If you're especially proud of this program, consider contributing to the list of example programs by sending the URL to <a href='mailto:&#114;&#101;&#118;&#64;&#118;&#97;&#100;&#103;&#46;&#105;&#111;?subject=" + encodeURIComponent(lang.toUpperCase() + ' Program') + "&body=" + encodeURIComponent("Hi! \n I made this cool " + lang.toUpperCase() + " program: " + url) + "' target='_blank'>&#114;&#101;&#118;&#64;&#118;&#97;&#100;&#103;&#46;&#105;&#111;</a>.</i></p>") ;
             });
     }
 
